c2824ca50fb8c89fa9ad7abeb4cc348f95a50974
Pedro <pedrofagiane4@gmail.com>
2024-06-17 13:53:57 -0300

    epa

--- js.js
+++ js.js
@@ -3,9 +3,13 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault(); 
         const Tar = $("#Tarefa").val();
-        const NovoI = $('<li></li>')
+        const NovoI = $('<li class="pedra"></li>')
         $(`<h2>${Tar}</h2>`).appendTo(NovoI)
         $(NovoI).appendTo('ul');
         $('#Tarrefa').val('');
     }) 
+
+    $('#pedra').click(function(e){
+        console.log(NovoI)
+    })
 })
