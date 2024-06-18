06f7ab7f9e5b2f632db2cffb0bdd24a91bf5f28e
Pedro <pedrofagiane4@gmail.com>
2024-06-18 11:08:20 -0300

    final

--- js.js
+++ js.js
@@ -7,12 +7,9 @@ $(document).ready(function(){
         $(`<button class="L">${Tar}</button>`).appendTo(NovoI)
         $(NovoI).appendTo('ul');
         $('#Tarrefa').val('');
-        $('.L').on('click', function(e){
-            $('.L').prop('disabled', true);
-            $('.L').css('text-decoration', 'line-through')
+    });
 
-            $('.L').prop('disabled', false)
-            $('.L').css('text-decoration', 'none');
-        });
-    })
+    $('ul').on('click', 'button.L', function(e){
+        $(this).toggleClass('done');
+    });
 })
